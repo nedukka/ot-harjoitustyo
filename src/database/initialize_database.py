6ffae17a2320541c6db4522e25a592eff9fa5b57
nedukka <nedukka11@gmail.com>
2026-04-01 01:21:45 +0300

    Add code for initializing and connecting to database

---
+++ src/database/initialize_database.py
@@ -0,0 +1,29 @@
+from .database_connection import get_database_connection
+
+def drop_tables(connection):
+    cursor = connection.cursor()
+    cursor.execute('''DROP TABLE IF EXISTS tasks''')
+    connection.commit()
+
+def create_tables(connection):
+    cursor = connection.cursor()
+    cursor.execute('''
+        CREATE TABLE tasks (
+            id INTEFER PRIMARY KEY,
+            title TEXT NOT NULL,
+            task_type TEXT NOT NULL,
+            completed INTEGER DEFAULT 0,
+            week_created INTEGER,
+            last_completed_week INTEGER
+        );
+    ''')
+
+    connection.commit()
+
+def initialize_database():
+    connection = get_database_connection()
+    drop_tables(connection)
+    create_tables(connection)
+
+if __name__ == '__main__':
+    initialize_database()
