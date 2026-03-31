4135478b4347e741aaa2e0a9b8b624d40192c5d8
nedukka <nedukka11@gmail.com>
2026-04-01 01:19:14 +0300

    Add TaskRepository class

---
+++ src/repositories/task_repository.py
@@ -0,0 +1,35 @@
+from database.database_connection import get_database_connection
+from models.task import Task
+
+class TaskRepository:
+    def __init__(self):
+        self.connection = get_database_connection()
+
+    def create_task(self, task: Task):
+        cursor = self.connection.cursor()
+        cursor.execute('''
+            INSERT INTO tasks (
+                   title, task_type, completed, week_created, last_completed_week)
+                    VALUES (?, ?, ?, ?, ?)
+        ''', (task.title, task.task_type, int(task.completed), task.week_created, task.last_completed_week))
+        self.connection.commit()
+
+        task.id = cursor.lastrowid
+        return task
+
+    def row_to_task(self, row):
+        return Task(
+            id=row['id'],
+            title=row['title'],
+            task_type=row['task_type'],
+            completed=bool(row['completed']),
+            week_created=row['week_created'],
+            last_completed_week=row['last_completed_week']
+        )
+
+    def get_all_tasks(self):
+        cursor = self.connection.cursor()
+        cursor.execute('SELECT * FROM tasks')
+        rows = cursor.fetchall()
+
+        return [self.row_to_task(row) for row in rows]
