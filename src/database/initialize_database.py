fbc223d4ff7f5e0d0f2ad7052e292322f29c26a9
nedukka <nedukka11@gmail.com>
2026-04-21 22:15:59 +0300

    Add users table to database initialization

--- src/database/initialize_database.py
+++ src/database/initialize_database.py
@@ -3,10 +3,18 @@ from src.database.database_connection import get_database_connection
 def drop_tables(connection):
     cursor = connection.cursor()
     cursor.execute('''DROP TABLE IF EXISTS tasks''')
+    cursor.execute('''DROP TABLE IF EXISTS users''')
     connection.commit()
 
 def create_tables(connection):
     cursor = connection.cursor()
+    cursor.execute('''
+        CREATE TABLE users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            nickname TEXT NOT NULL
+        );
+    ''')
+
     cursor.execute('''
         CREATE TABLE tasks (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
